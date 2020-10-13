f67b4d03c38473b94af27bbf7823306e6c21c7aa
avagford <71782282+avagford@users.noreply.github.com>
2020-10-13 15:44:54 -0700

    First commit!!!

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  forfeitStory
+//
+//  Created by Ava Ford on 10/12/20.
+//  Copyright © 2020 Ava Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
